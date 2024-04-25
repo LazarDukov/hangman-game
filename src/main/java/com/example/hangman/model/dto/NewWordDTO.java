78e4d1201ab4e8642265cbad70f596e053e76d35
LazarDukov <lazarrumenowdukow@gmail.com>
2024-04-25 23:49:28 +0300

    add word page created but should create a validations for it

--- src/main/java/com/example/hangman/model/dto/NewWordDTO.java
+++ src/main/java/com/example/hangman/model/dto/NewWordDTO.java
@@ -2,11 +2,19 @@ package com.example.hangman.model.dto;
 
 import com.example.hangman.model.entity.Category;
 import com.example.hangman.model.entity.Difficulty;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 
 public class NewWordDTO {
+    @NotNull(message = "This field cannot be empty!")
+    @Size(min = 2, max = 20, message = "Size must be between 2 and 20 symbols!")
     private String word;
+    @NotNull(message = "This field cannot be empty!")
+    @Size(min = 2, max = 20, message = "Size must be between 20 and 150 symbols!")
     private String description;
+
     private String category;
     private String difficulty;
 
